26c3de48c57a7b751e99af4d4c6817df22731be6
Jupiter <sophy8835@hotmail.com>
2023-10-18 22:54:17 +0900

    1018

--- app/page.tsx
+++ app/page.tsx
@@ -3,14 +3,24 @@ import Link from "next/link";
 import ProductCard from "./components/ProductCard";
 import { getServerSession } from "next-auth";
 import { authOptions } from "./api/auth/[...nextauth]/route";
+import alc from "@/public/images/111.jpg";
 
 export default async function Home() {
   const session = await getServerSession(authOptions);
   return (
-    <main>
+    <main className="relative h-screen">
       <h1>hello!!!{session && <span>{session.user!.name}</span>}</h1>
       <Link href="/users">Users</Link>
       <ProductCard></ProductCard>
+
+      <Image
+        src="https://bit.ly/react-cover"
+        alt="alc"
+        fill
+        className="object-cover"
+        sizes="(max-width:480px) 100vw,(max-width:768px) 50vw,33vw"
+        quality={100}
+      />
     </main>
   );
 }
