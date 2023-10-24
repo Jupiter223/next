bbf8c6aa0874ad3605c7f3a96ab1825cbbf52f8c
Jupiter <sophy8835@hotmail.com>
2023-10-24 22:55:46 +0900

    1024

---
+++ app/components/HeavyComponent.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const HeavyComponent = () => {
+  return <div>HC</div>;
+};
+
+export default HeavyComponent;
--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,4 @@
+"use client";
 import Image from "next/image";
 import Link from "next/link";
 import ProductCard from "./components/ProductCard";
@@ -6,22 +7,42 @@ import { authOptions } from "./api/auth/[...nextauth]/route";
 import alc from "@/public/images/111.jpg";
 import { Metadata } from "next";
 
-export default async function Home() {
-  const session = await getServerSession(authOptions);
+// import dynamic from "next/dynamic";
+import { useState } from "react";
+// import _ from "lodash";
+// const HeavyComponent = dynamic(() => import("./components/HeavyComponent"), {
+//   ssr: false,
+//   loading: () => <p>Loading</p>,
+// });
+export default function Home() {
+  // const session = await getServerSession(authOptions);
+  const [isVisible, setVisible] = useState(false);
   return (
     <main className="relative h-screen">
-      <h1>hello!!!{session && <span>{session.user!.name}</span>}</h1>
+      {/* <h1>hello!!!{session && <span>{session.user!.name}</span>}</h1> */}
+      <button
+        onClick={async () => {
+          const _ = (await import("lodash")).default;
+          const users = [{ name: "c" }, { name: "b" }, { name: "a" }];
+          const sorted = _.orderBy(users, ["name"]);
+          console.log(sorted);
+        }}
+      >
+        Set
+      </button>
+      {/* {isVisible && <HeavyComponent />} */}
+
       <Link href="/users">Users</Link>
       <ProductCard></ProductCard>
 
-      <Image
+      {/* <Image
         src="https://bit.ly/react-cover"
         alt="alc"
         fill
         className="object-cover"
         sizes="(max-width:480px) 100vw,(max-width:768px) 50vw,33vw"
         quality={100}
-      />
+      /> */}
     </main>
   );
 }
@@ -30,10 +51,10 @@ export default async function Home() {
 //   title: "...",
 // };
 
-export async function generateMetadata(): Promise<Metadata> {
-  const product = await fetch("");
-  return {
-    title: "product.title",
-    description: "...",
-  };
-}
+// export async function generateMetadata(): Promise<Metadata> {
+//   const product = await fetch("");
+//   return {
+//     title: "product.title",
+//     description: "...",
+//   };
+// }
